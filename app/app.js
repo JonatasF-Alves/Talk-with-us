9f6a06434048cbaf25c2f289d86244ec727c1bcf
Jonatas <jonatas32443048.com>
2026-04-09 17:01:34 -0300

    socket.io

--- app/app.js
+++ app/app.js
@@ -1,26 +1,10 @@
-const socket = new WebSocket('ws://localhost:3000');
-
-socket.addEventListener('open', () => {
-    console.log('WebSocket conectado');
-});
-
-socket.addEventListener('error', event => {
-    console.error('WebSocket error:', event);
-});
-
-socket.addEventListener('close', () => {
-    console.warn('WebSocket fechado');
-});
+const socket = io('ws://localhost:3500');
 
 function sendMenssage(e) {
     e.preventDefault();
     const input = document.querySelector('input');
     if (input.value) {
-        if (socket.readyState !== WebSocket.OPEN) {
-            console.error('Conexão WebSocket não está aberta');
-            return;
-        }
-        socket.send(input.value);
+        socket.emit('message', input.value);
         input.value = "";
     }
     input.focus();
@@ -30,7 +14,7 @@ document.querySelector('form')
     .addEventListener('submit', sendMenssage);
 
 // listen for messages
-socket.addEventListener('message', ({ data }) => {
+socket.on('message', (data) => {
     const li = document.createElement('li');
     li.textContent = data;
     document.querySelector('ul').appendChild(li);
